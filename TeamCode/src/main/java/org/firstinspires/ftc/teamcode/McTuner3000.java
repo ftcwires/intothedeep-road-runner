f3e47b9b7693b521f354c63aa1afcd9bf2ee932a
BudsterGaming <budstergaming@binaryflood.com>
2023-11-05 10:38:03 -0700

    Making the todo list for McTuner3000 was unnecessary, fun, but unnecessary.
    -goober

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/McTuner3000.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/McTuner3000.java
@@ -17,6 +17,7 @@ public class McTuner3000 extends LinearOpMode {
 
     double speedAmount;
 
+    //TODO: Step 3, set all of your servo names below
     enum ServoTypes{
         SHOULDER,
         HOPPER,
@@ -24,6 +25,7 @@ public class McTuner3000 extends LinearOpMode {
         LIFT;
     }
     ServoTypes which;
+    //TODO: Step 4, replace all names of Servos with yours, and replace all capitals with what you set them to from step 3
 
     private void masterTuner() {
         if (gamepad1.left_bumper) {
@@ -57,6 +59,7 @@ public class McTuner3000 extends LinearOpMode {
             }
         }
     }
+    //TODO: Step 5, replace all of your Servo functions below
     private void setServo() {
         if (gamepad1.y) {
             which = ServoTypes.SHOULDER;
@@ -87,6 +90,8 @@ public class McTuner3000 extends LinearOpMode {
         }
         telemetry.addData("Speed = ", speedAmount);
     }
+    //TODO: Step 6 replace all of the xyz.getPosition()0; with your servos and replace "xyz" with what that servo is
+    //TODO: Step 7, your done! This was written by Goober on 11/5/23 slouching in a chair at 10:35 in the morning. And as I'm writing this I wonder if anybody will actually use this. Problably not, but idk what to do while I wait for the robot to be ready for calibration. It's now 10:36 am. I wonder if future me will use this and or remember doing this. Goober out.
     private void whatServoAt() {
         telemetry.addData("Shoulder = ", shoulder.getPosition());
         telemetry.addData("Hopper = ", hopper.getPosition());
