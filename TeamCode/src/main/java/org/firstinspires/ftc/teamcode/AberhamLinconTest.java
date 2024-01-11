f762fd8a630932342519d712c29860488ac82720
BudsterGaming <budstergaming@binaryflood.com>
2024-01-10 17:04:00 -0700

    pushing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AberhamLinconTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class AberhamLinconTest extends LinearOpMode {
+    private ColorSensor color;
+
+    private void function() {
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        color = hardwareMap.get(ColorSensor.class, "color");
+
+        telemetry.update();
+        waitForStart();
+        color.enableLed(true);
+
+        while(opModeIsActive()) {
+
+
+            telemetry.update();
+        }
+    }
+}
