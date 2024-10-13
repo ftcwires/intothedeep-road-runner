fa7a0c8184f8a860186ec5240cb80e51712c717c
amjadj <amjad.jabs@gmail.com>
2024-10-12 19:43:00 -0500

    FTC Wires Autonomous Mode for IntoTheDeep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreeDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreeDeadWheelLocalizer.java
@@ -16,9 +16,11 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 @Config
 public final class ThreeDeadWheelLocalizer implements Localizer {
     public static class Params {
+        //TODO Step 11.1 : Update values of par0YTicks, part1YTicks, perpXTicks from AngularRampLogger
         public double par0YTicks = 0.0; // y position of the first parallel encoder (in tick units)
         public double par1YTicks = 1.0; // y position of the second parallel encoder (in tick units)
         public double perpXTicks = 0.0; // x position of the perpendicular encoder (in tick units)
+        //TODO End of Step 11.1
     }
 
     public static Params PARAMS = new Params();
@@ -30,12 +32,21 @@ public final class ThreeDeadWheelLocalizer implements Localizer {
     private int lastPar0Pos, lastPar1Pos, lastPerpPos;
 
     public ThreeDeadWheelLocalizer(HardwareMap hardwareMap, double inPerTick) {
+        //TODO Step 3.1 : Update hardware configuration names for dead wheel encoders
         // TODO: make sure your config has **motors** with these names (or change them)
         //   the encoders should be plugged into the slot matching the named motor
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        par0 = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "par0")));
-        par1 = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "par1")));
-        perp = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "perp")));
+        // Example / Recommendation:
+        // Have Port 0 connected to Left Front Motor & one of the parallel dead wheel encoder,
+        // change device name: "par0" here and "leftFront" in MecanumDrive class changed to "leftFront_par0"
+        // Have Port 3 connected to Left Back Motor & the other parallel dead wheel encoder,
+        // change device name: "par1" here and "leftFront" in MecanumDrive class changed to "leftBack_par1"
+        // Have Port 1 connected to Right Front Motor & the perpendicular dead wheel encoder,
+        // change device name: "perp" here and "rightFront" in MecanumDrive class changed to "rightFront_perp"
+        par0 = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "leftFront_par0")));
+        par1 = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "leftBack_par1")));
+        perp = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "rightFront_perp")));
+        //TODO End of Step 3.1
 
         lastPar0Pos = par0.getPositionAndVelocity().position;
         lastPar1Pos = par1.getPositionAndVelocity().position;
